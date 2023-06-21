7a30c262aefe760b76580bec20a24a7ab7a8ab19
Rock N Rome <rocknrome21@gmail.com>
2023-06-21 13:49:24 -0400

    Lvl_3 JS added

--- starter/level-three.js
+++ starter/level-three.js
@@ -2,3 +2,27 @@
  * DOTS: Level Three
  *
  */
+//Global variables setup
+let score = 0;
+const balls = document.querySelectorAll('.js-ball');
+const scoreDisplay = document.querySelector('.js-score');
+const levelWinner = document.querySelector('.level-winner');
+
+function handleBallClick(increment) {
+  score += increment;
+  scoreDisplay.innerText = `${score}`;
+
+  if (score === 100) {
+    levelWinner.style.opacity = '1';
+  } else {
+    levelWinner.style.opacity = '0'; // Ensure opacity is set to 0 when the condition is not met
+  }
+}
+
+balls.forEach((ball) => {
+  const increment = parseInt(ball.dataset.increment);
+
+  ball.addEventListener('click', () => {
+    handleBallClick(increment);
+  });
+});
--- starter/level-two.js
+++ starter/level-two.js
@@ -8,8 +8,8 @@ const balls = document.querySelectorAll('.js-ball');
 const scoreDisplay = document.querySelector('.js-score');
 const levelWinner = document.querySelector('.level-winner');
 
-console.log(score, balls, scoreDisplay, levelWinner); // Checkpoint
-console.log(typeof score, typeof balls, typeof scoreDisplay, typeof levelWinner); // Checkpoint
+//console.log(score, balls, scoreDisplay, levelWinner); // Checkpoint
+//console.log(typeof score, typeof balls, typeof scoreDisplay, typeof levelWinner); // Checkpoint
 
 // Functionality
 function handleBallClick(ball) { // Function to handle the ball click event
